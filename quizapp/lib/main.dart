837353a77a10f9ef17039b924f11b068fdce5423
SK1LLEDN1NJA <augustineantonyjoy@outlook.com>
2021-05-12 22:40:05 +0530

    added buttons

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestions() {
+    print('Answer Selected');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('Quiz App development in progress.'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestions),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestions),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestions),
+          ],
+        ),
       ),
     );
   }
